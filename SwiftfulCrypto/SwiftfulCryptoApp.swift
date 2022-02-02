b0fb34db2fbdc18c60e36026b7bfd7d8329e42df
Guillaume Ramey <guillaume.ramey@gmail.com>
2022-02-02 14:06:00 +0100

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Guillaume Ramey on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
